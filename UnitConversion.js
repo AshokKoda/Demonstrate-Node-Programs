ad0648758b219e3ff792298f88f3d9232a9eb992
Ashok Koda <a.koda59@gmail.com>
2022-01-16 12:16:23 +0530

    Added:Unit conversion

---
+++ UnitConversion.js
@@ -0,0 +1,16 @@
+//1ft = 12 in then 42 in = ? ft 
+const oneFeet = 12;
+const inch = 42;
+
+var tolFeet = oneFeet / 4;
+console.log("Total 42 inch = " + tolFeet + " feet");
+
+//Rectangular Plot of 60 feet x 40 feet in meters
+const length = 60;
+const width = 40;
+
+console.log("The length of the rectangle: " + length * 0.3048);
+console.log("The width of the rectangle: " + width * 0.3048);
+
+var sqft = length * width;
+console.log("The area of the square feet: " + sqft);
