86c1a1447866b4dbe14e66602d4da1ee606f5867
Ashok Koda <a.koda59@gmail.com>
2022-01-16 11:08:29 +0530

    Added:Add 5 random numbers with 2 digits

---
+++ 5Random2Digits.js
@@ -0,0 +1,15 @@
+var num1 = Math.floor(Math.random() * 90) + 10;
+var num2 = Math.floor(Math.random() * 90) + 10;
+var num3 = Math.floor(Math.random() * 90) + 10;
+var num4 = Math.floor(Math.random() * 90) + 10;
+var num5 = Math.floor(Math.random() * 90) + 10;
+
+console.log("The number is " + num1);
+console.log("The number is " + num2);
+console.log("The number is " + num3);
+console.log("The number is " + num4);
+console.log("The number is " + num5);
+
+var sum = num1 + num2 + num3 + num4 + num5;
+console.log("Sum = " + sum);
+console.log("Average = " + sum/5);
