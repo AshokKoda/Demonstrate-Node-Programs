16aa904a16bc571e4e6f32ef5f43dd991c1dd6f6
Ashok Koda <a.koda59@gmail.com>
2022-01-19 10:47:10 +0530

    Added: Sequence and selection

---
+++ Sequence/5Random3Digits.js
@@ -0,0 +1,16 @@
+var num1 = Math.floor(Math.random() * 900) + 100;
+var num2 = Math.floor(Math.random() * 900) + 100;
+var num3 = Math.floor(Math.random() * 900) + 100;
+var num4 = Math.floor(Math.random() * 900) + 100;
+var num5 = Math.floor(Math.random() * 900) + 100;
+
+console.log("The number is " + num1);
+console.log("The number is " + num2);
+console.log("The number is " + num3);
+console.log("The number is " + num4);
+console.log("The number is " + num5);
+
+var sum = num1 + num2 + num3 + num4 + num5;
+console.log("Sum = " + sum);
+console.log("Max Value = " + Math.max(sum));
+console.log("Min Value = " + Math.min(sum));
